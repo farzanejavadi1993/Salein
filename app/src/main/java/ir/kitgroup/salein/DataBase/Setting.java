0bec2c67706e527c6836a859a8543f74ce3db94e
f.javadi <Farzane1!>
2021-09-15 09:52:53 +0430

    whole project 1

---
+++ app/src/main/java/ir/kitgroup/salein/DataBase/Setting.java
@@ -0,0 +1,9 @@
+package ir.kitgroup.salein.DataBase;
+
+import com.orm.SugarRecord;
+
+public class Setting extends SugarRecord {
+    public String DEFAULT_PRICE_INVOICE;
+    public String MAX_SALE;
+    public String DEFAULT_CUSTOMER;
+}
---
+++ app/src/main/java/ir/kitgroup/salein/models/ModelProduct.java
@@ -0,0 +1,45 @@
+package ir.kitgroup.salein.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+import ir.kitgroup.salein.DataBase.ProductGroupLevel1;
+import ir.kitgroup.salein.DataBase.ProductGroupLevel2;
+
+public class ModelProduct {
+
+    @SerializedName("ProductLevel1")
+    @Expose
+    private List<ProductGroupLevel1> ProductLevel1List = null;
+
+    @SerializedName("ProductLevel2")
+    @Expose
+    private List<ProductGroupLevel2> ProductLevel2List = null;
+
+    @SerializedName("Product")
+    @Expose
+    private List<ir.kitgroup.salein.DataBase.Product> ProductList = null;
+
+    public List<ProductGroupLevel1> getProductLevel1List() {
+        return ProductLevel1List;
+    }
+    public void setProductLevel1List(List<ProductGroupLevel1> productLevel1List) {
+        this.ProductLevel1List = productLevel1List;
+    }
+
+    public List<ProductGroupLevel2> getProductLevel2List() {
+        return ProductLevel2List;
+    }
+    public void setProductLevel2List(List<ProductGroupLevel2> productLevel2List) {
+        this.ProductLevel2List = productLevel2List;
+    }
+
+    public List<ir.kitgroup.salein.DataBase.Product> getProductList() {
+        return ProductList;
+    }
+    public void setProductList(List<ir.kitgroup.salein.DataBase.Product> productList) {
+        this.ProductList = productList;
+    }
+}
