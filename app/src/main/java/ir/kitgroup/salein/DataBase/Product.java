19da29a73f59eee799fb6a4ffa0b2e2431ce8bfb
f.javadi <Farzane1!>
2021-09-26 09:07:08 +0330

    whole project 1

---
+++ app/src/main/java/ir/kitgroup/salein/DataBase/Product.java
@@ -0,0 +1,112 @@
+package ir.kitgroup.salein.DataBase;
+
+
+
+import com.orm.SugarRecord;
+import com.orm.dsl.Unique;
+
+import ir.kitgroup.salein.Util.Util;
+
+public class Product extends SugarRecord {
+    @Unique
+    public String I;
+    public String PID1;
+    public String PID2;
+    public String PID3;
+    public String N;//name
+    public String NIP;//name KHOLASE
+    public Double PU1;
+    public Double PU2;
+    public Double PU3;
+    public Double PU4;
+    public Double PU5;
+    public String DES;
+    public Boolean TAX;
+    public Boolean STS;
+    public Double PERC_DIS;
+    public String IMG;
+    public String descItem;
+
+    public int MS;//MAX
+
+    public Boolean getSTS() {
+      return STS;
+
+    }
+
+    public void setSTS(Boolean STS) {
+        this.STS = STS;
+    }
+
+    public Boolean star;
+
+    public String getPRDUID() {
+        return I;
+    }
+    public String getPRDLVLUID2() {
+        return PID2;
+    }
+    public String getPRDNAME() {
+        return N;
+    }
+    public Double getPRDPRICEPERUNIT1() {
+        Double showPrice = 0.0;
+        try {
+
+            String priceList = Util.getPrice();
+
+            switch (priceList) {
+                case "2":
+                    showPrice = PU2;
+                    break;
+                case "3":
+                    showPrice = PU3;
+                    break;
+                case "4":
+                    showPrice = PU4;
+                    break;
+                case "5":
+                    showPrice = PU5;
+                    break;
+                default:
+                    showPrice = PU1;
+                    break;
+            }
+
+
+
+        } catch (Exception ignored) {
+        }
+        return showPrice;
+    }
+    public Double getPRDPERCENTDISCOUNT() {
+        return PERC_DIS;
+    }
+
+    public Double AMOUNT;
+
+    public Boolean getStar() {
+        return star;
+    }
+
+    public void setStar(Boolean star) {
+        this.star = star;
+    }
+
+    public Double getAmount() {
+        if (AMOUNT==null) {
+            return 0.0;
+        } else {
+            return AMOUNT;
+        }
+
+    }
+
+    public void setAmount(Double amount1) {
+
+        this.AMOUNT = amount1;
+    }
+
+    public Product(){}
+
+}
---
+++ app/src/main/java/ir/kitgroup/salein/Fragments/MobileView/SplashScreenFragment.java
@@ -0,0 +1,174 @@
+package ir.kitgroup.salein.Fragments.MobileView;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.orm.query.Select;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+import ir.kitgroup.salein.Activities.Classes.LauncherActivity;
+import ir.kitgroup.salein.DataBase.Account;
+import ir.kitgroup.salein.DataBase.User;
+import ir.kitgroup.salein.Fragments.LauncherOrganizationFragment;
+import ir.kitgroup.salein.Fragments.LoginOrganizationFragment;
+import ir.kitgroup.salein.R;
+import ir.kitgroup.salein.Util.Util;
+import ir.kitgroup.salein.classes.App;
+import ir.kitgroup.salein.databinding.FragmentSplashScreenBinding;
+
+public class SplashScreenFragment extends Fragment {
+
+    private int imageIconDialog = 0;
+    private String title = "";
+    private FragmentSplashScreenBinding binding;
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+
+        binding = FragmentSplashScreenBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // region Utilize Animation
+        Util.playLottieAnimation("buygirl.json", binding.animationView);
+        //Util.playLottieAnimation("890-loading-animation.json", binding.animationView1);
+        //endregion Utilize Animation
+
+        //region Set Icon And Title
+        switch (LauncherActivity.name) {
+            case "ir.kitgroup.salein":
+                imageIconDialog = R.drawable.salein;
+                title = "سالین دمو";
+                break;
+
+            case "ir.kitgroup.saleintop":
+
+                imageIconDialog = R.drawable.top_png;
+                title = "تاپ کباب";
+
+                break;
+
+
+            case "ir.kitgroup.saleinmeat":
+
+                imageIconDialog = R.drawable.meat_png;
+                title = "گوشت دنیوی";
+                break;
+            case "ir.kitgroup.saleinnoon":
+
+                imageIconDialog = R.drawable.noon;
+                title = "کافه نون";
+                break;
+        }
+
+        try {
+            String appVersion = appVersion();
+            binding.tvversion.setText(" نسخه " + appVersion);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        binding.imgLogo.setImageResource(imageIconDialog);
+        binding.tvTitle.setText(title);
+        //endregion Set Icon And Title
+
+        //region Thread
+
+        Thread thread = new Thread(() -> {
+            try {
+
+                Thread.sleep(5000);
+
+                FragmentTransaction replaceFragment;
+
+                //regionClient Application
+                if (App.mode == 2) {
+
+                    //region Account Is Login & Register
+                    if (Select.from(Account.class).list().size() > 0) {
+
+                        //regionShow All Company
+                        if (LauncherActivity.name.equals("ir.kitgroup.salein"))
+                            replaceFragment = getActivity().getSupportFragmentManager().beginTransaction().add(R.id.frame_main, new StoriesFragment(), "StoriesFragment").addToBackStack("StoriesF");
+
+
+                            //endregionShow All Company
+
+                        //region Go To MainOrderFragment Because Account Is Register
+                        else {
+                            Bundle bundle = new Bundle();
+                            bundle.putString("Ord_TYPE", "");
+                            bundle.putString("Tbl_GUID", "");
+                            bundle.putString("Inv_GUID", "");
+                            MainOrderMobileFragment mainOrderMobileFragment = new MainOrderMobileFragment();
+                            mainOrderMobileFragment.setArguments(bundle);
+                            replaceFragment = Objects.requireNonNull(getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame_main, mainOrderMobileFragment, "MainOrderMobileFragment"));
+                        }
+                        //endregion Go To MainOrderFragment Because Account Is Register
+
+                    }
+                    //endregion Account Is Login & Register
+
+
+                    //region Account Is Not Login & Register
+                    else {
+
+                        replaceFragment = getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame_main, new LoginClientFragment());
+
+                    }
+                    //endregion Account Is Not Login & Register
+                }
+                //endregionClient Application
+
+
+                //regionOrganization Application
+                else {
+                    //When User Is Login
+                    if (Select.from(User.class).list().get(0).CheckUser) {
+                        replaceFragment = getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame_main, new LauncherOrganizationFragment(), "LauncherFragment");
+                    }
+
+                    //When User Is Not Login
+                    else {
+
+                        replaceFragment = getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame_main, new LoginOrganizationFragment());
+
+                    }
+
+                }
+                //endregionOrganization Application
+
+                replaceFragment.commit();
+
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+        //endregion Thread
+    }
+
+    public String appVersion() throws PackageManager.NameNotFoundException {
+        PackageInfo pInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
+        return pInfo.versionName;
+    }
+}
