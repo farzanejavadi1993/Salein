90d6c3ad834cfd43ece135a300847825979671c9
f.javadi <Farzane1!>
2021-10-12 13:35:57 +0330

    whole project 1

---
+++ app/src/main/java/ir/kitgroup/saleinmeat/DataBase/Setting.java
@@ -0,0 +1,17 @@
+package ir.kitgroup.saleinmeat.DataBase;
+
+import com.orm.SugarRecord;
+import com.orm.dsl.Unique;
+
+public class Setting extends SugarRecord {
+    @Unique
+    public String DEFAULT_PRICE_INVOICE;
+    public String MAX_SALE;
+    public String DEFAULT_CUSTOMER;
+    public String ORDER_TYPE_APP;
+    public String SERVICE_TIME;
+    public String UPDATE_APP;
+    public String VERSION_APP;
+    public String SERVICE_DAY;
+    public String PEYK;
+}
