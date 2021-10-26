a5ff339e34bcc6f40845aa2db8c7fb8deaa01331
f.javadi <Farzane1!>
2021-10-26 15:30:43 +0330

    SaleIn

---
+++ app/src/main/java/ir/kitgroup/saleintop/models/ModelAccount.java
@@ -0,0 +1,26 @@
+package ir.kitgroup.saleintop.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+import ir.kitgroup.saleintop.DataBase.Account;
+
+public class ModelAccount {
+
+    @SerializedName("Account")
+    @Expose
+    private List<Account> AccountList = null;
+
+
+
+    public List<Account> getAccountList() {
+        return AccountList;
+    }
+    public void setAccountList(List<Account> AccountList) {
+        this.AccountList = AccountList;
+    }
+
+
+}
