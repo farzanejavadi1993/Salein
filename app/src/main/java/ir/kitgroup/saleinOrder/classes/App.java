9e50cd12c9c6e7cca25ac6bdaa9ee9cfd3dcd415
f.javadi <Farzane1!>
2021-11-01 10:06:05 +0330

    SaleIn

---
+++ app/src/main/java/ir/kitgroup/saleinOrder/classes/App.java
@@ -0,0 +1,43 @@
+package ir.kitgroup.saleinOrder.classes;
+
+import android.app.Application;
+
+
+import com.cedarstudios.cedarmapssdk.CedarMaps;
+import com.cedarstudios.cedarmapssdk.model.MapID;
+import com.orm.SugarContext;
+
+import dagger.hilt.android.HiltAndroidApp;
+import ir.kitgroup.saleinOrder.Connect.API;
+import retrofit2.Retrofit;
+
+@HiltAndroidApp
+public class App extends Application {
+
+
+
+    public static API api;
+    public static Retrofit retrofit;
+
+
+
+
+
+
+
+    @Override
+    public void onCreate() {
+        SugarContext.init(getApplicationContext());
+
+
+        CedarMaps.getInstance()
+                .setClientID("sportapp-6594917192157661130")
+                .setClientSecret("b2uejHNwb3J0YXBw4V7hZnhRDiV3fQ8aqbTay-mjSd1IXllmWRN1EezGsss=")
+                .setContext(this)
+                .setMapID(MapID.MIX);
+
+
+
+        super.onCreate();
+    }
+}
