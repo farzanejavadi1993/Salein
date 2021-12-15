a89b2c5a62e69cbf6cd853d56ff408eb53edd7b4
f.javadi <Farzane1!>
2021-12-15 15:05:59 +0330

    SaleIn

---
+++ app/src/main/java/ir/kitgroup/saleinOrder/Adapters/OrderListAdapter.java
@@ -0,0 +1,172 @@
+package ir.kitgroup.saleinOrder.Adapters;
+
+
+import android.annotation.SuppressLint;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import org.jetbrains.annotations.NotNull;
+
+
+import java.text.DecimalFormat;
+
+import java.util.List;
+import java.util.Objects;
+
+import ir.kitgroup.saleinOrder.models.Invoice;
+import ir.kitgroup.saleinOrder.R;
+
+
+
+public class OrderListAdapter extends RecyclerView.Adapter<OrderListAdapter.viewHolder> {
+
+
+    private final List<Invoice> list;
+    private final Context context;
+    private final DecimalFormat format = new DecimalFormat("#,###,###,###");
+
+
+    public void setOnClickItemListener(ClickItem clickItem) {
+        this.clickItem = clickItem;
+    }
+
+    public interface ClickItem {
+        void onRowClick(String description,int type, String Inv_GUID);
+    }
+
+    private ClickItem clickItem;
+
+
+    public OrderListAdapter(Context context, List<Invoice> list) {
+        this.context = context;
+        this.list = list;
+
+
+
+    }
+
+    @Override
+    public @NotNull viewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.order_list_item_mobile, parent, false);
+        return new viewHolder(view);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(final viewHolder holder, final int position) {
+
+
+        Invoice invoice = list.get(holder.getAdapterPosition());
+
+        if (invoice.invFinalStatusControl) {
+            holder.tvStatus.setText("آماده سازی برای ارسال");
+            holder.tvStatus.setTextColor(context.getResources().getColor(R.color.orange));
+        }
+       else if (invoice.INV_SYNC.equals("*")) {
+            holder.tvStatus.setText("ارسال شده");
+            holder.tvStatus.setTextColor(context.getResources().getColor(R.color.green_table));
+        } else if (invoice.INV_SYNC.equals("-")) {
+            holder.tvStatus.setText("در انتظار بررسی اپراتور");
+            holder.tvStatus.setTextColor(context.getResources().getColor(R.color.blue600));
+        } else {
+            holder.tvStatus.setText("ناموفق");
+            holder.tvStatus.setTextColor(context.getResources().getColor(R.color.red_table));
+        }
+
+
+        holder.tvTotalPrice.setText(format.format((invoice.INV_EXTENDED_AMOUNT)) + " ریال ");
+
+
+        //تاریخ تحویل
+        if (invoice.INV_DUE_DATE_PERSIAN != null)
+            holder.tvDeliveryDateOrder.setText(invoice.INV_DUE_TIME +"_"+invoice.INV_DUE_DATE_PERSIAN );
+
+
+        //تاریخ ارسال
+        holder.tvDateOrder.setText(invoice.INV_DATE_PERSIAN);
+
+
+        Objects.requireNonNull(holder.itemView).setOnClickListener(view -> clickItem.onRowClick("",1, invoice.INV_UID));
+
+
+        holder.ivSend.setOnClickListener(v ->{
+            if (holder.edtDescription.getText().toString().equals(""))
+            {
+                AlertDialog alertDialog=  new AlertDialog.Builder(context)
+                        .setMessage("هیچ نظری موجود نمی باشد")
+                        .setPositiveButton("بستن", (dialog, which) -> {
+                            dialog.dismiss();
+                        })
+                        .show();
+
+                TextView textView = (TextView) alertDialog.findViewById(android.R.id.message);
+                Typeface face=Typeface.createFromAsset(context.getAssets(), "iransans.ttf");
+                textView.setTypeface(face);
+                textView.setTextColor(context.getResources().getColor(R.color.red_table));
+                textView.setTextSize(13);
+
+                return;
+            }
+
+                    clickItem.onRowClick(holder.edtDescription.getText().toString(), 2, invoice.INV_UID);
+
+    });
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return list.size();
+    }
+
+    static class viewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView tvTotalPrice;
+        private final TextView tvDateOrder;
+        private final TextView tvStatus;
+        private final TextView tvDeliveryDateOrder;
+        private final ImageView ivSend;
+        private final EditText edtDescription;
+
+
+
+        public viewHolder(View itemView) {
+            super(itemView);
+            tvTotalPrice = itemView.findViewById(R.id.tv_total_price);
+
+            tvDateOrder = itemView.findViewById(R.id.tv_date_order);
+            edtDescription = itemView.findViewById(R.id.edt_description);
+            ivSend = itemView.findViewById(R.id.iv_send);
+            tvDeliveryDateOrder = itemView.findViewById(R.id.tv_date_delivery_order);
+            tvStatus = itemView.findViewById(R.id.tv_status);
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+}
+
+
+
+
+
---
+++ app/src/main/java/ir/kitgroup/saleinOrder/Fragments/OrderListFragment.java
@@ -0,0 +1,444 @@
+package ir.kitgroup.saleinOrder.Fragments;
+
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import androidx.fragment.app.Fragment;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+
+import com.google.android.material.button.MaterialButton;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.orm.query.Select;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
+import ir.kitgroup.saleinOrder.Activities.LauncherActivity;
+import ir.kitgroup.saleinOrder.Adapters.OrderListAdapter;
+
+import ir.kitgroup.saleinOrder.Connect.API;
+import ir.kitgroup.saleinOrder.DataBase.Account;
+import ir.kitgroup.saleinOrder.DataBase.InvoiceDetail;
+
+import ir.kitgroup.saleinOrder.classes.ConfigRetrofit;
+import ir.kitgroup.saleinOrder.classes.CustomProgress;
+import ir.kitgroup.saleinOrder.DataBase.Company;
+import ir.kitgroup.saleinOrder.models.Invoice;
+
+
+import ir.kitgroup.saleinOrder.R;
+import ir.kitgroup.saleinOrder.classes.Util;
+
+import ir.kitgroup.saleinOrder.classes.DateConverter;
+import ir.kitgroup.saleinOrder.databinding.FragmentOrderListBinding;
+import ir.kitgroup.saleinOrder.models.ModelInvoice;
+
+import ir.kitgroup.saleinOrder.models.ModelLog;
+import ir.kitgroup.saleinOrder.models.PaymentRecieptDetail;
+
+
+@AndroidEntryPoint
+public class OrderListFragment extends Fragment {
+
+
+    private API api;
+
+    private Company company;
+
+
+    @Inject
+    SharedPreferences sharedPreferences;
+
+    @Inject
+    Typeface typeface;
+
+    //region Parameter
+    private CustomProgress customProgress;
+    private CompositeDisposable compositeDisposable;
+
+    private FragmentOrderListBinding binding;
+
+    //region Dialog Sync
+    private Dialog dialogSync;
+    private TextView textMessageDialog;
+    private ImageView ivIconSync;
+    private MaterialButton btnOkDialog;
+    private MaterialButton btnNoDialog;
+    //endregion Dialog Sync
+
+
+    //endregion Parameter
+
+    public OrderListAdapter orderListAdapter;
+    private final ArrayList<Invoice> list = new ArrayList<>();
+
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @org.jetbrains.annotations.NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        binding = FragmentOrderListBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        ((LauncherActivity) getActivity()).getVisibilityBottomBar(false);
+        compositeDisposable = new CompositeDisposable();
+        customProgress = CustomProgress.getInstance();
+
+        String accGUID = Select.from(Account.class).list().get(0).I;
+
+
+            company = null;
+            api = null;
+            company = Select.from(Company.class).first();
+        api = ConfigRetrofit.getRetrofit("http://" + company.IP1 + "/api/REST/",false).create(API.class);
+
+
+
+
+        //region Calculate Date Always Product
+
+
+        int dayAlways = 100;
+        try {
+            dayAlways = Integer.parseInt("700");
+        } catch (Exception ignore) {
+        }
+        Date date = Calendar.getInstance().getTime();
+        @SuppressLint("SimpleDateFormat") DateFormat dateFormats =
+                new SimpleDateFormat("dd/MM/yyyy");
+        Date newDate = Util.deleteDays(date, -dayAlways);
+        String d = dateFormats.format(newDate);
+        String[] da = d.split("/");
+        DateConverter converter = new DateConverter();
+        String datVip = converter.gregorianToPersian(Integer.parseInt(da[2]), Integer.parseInt(da[1]), Integer.parseInt(da[0]));
+        //endregion Calculate Date Always Product
+
+
+        //region Cast Variable Dialog Sync
+        dialogSync = new Dialog(getActivity());
+        dialogSync.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialogSync.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialogSync.setContentView(R.layout.custom_dialog);
+        dialogSync.setCancelable(false);
+
+        textMessageDialog = dialogSync.findViewById(R.id.tv_message);
+        ivIconSync = dialogSync.findViewById(R.id.iv_icon);
+
+        btnOkDialog = dialogSync.findViewById(R.id.btn_ok);
+        btnNoDialog = dialogSync.findViewById(R.id.btn_cancel);
+        btnNoDialog.setOnClickListener(v -> dialogSync.dismiss());
+
+
+        btnOkDialog.setOnClickListener(v -> {
+            dialogSync.dismiss();
+            getAllInvoice1(accGUID, datVip);
+
+        });
+
+        //endregion Cast Variable Dialog Sync
+
+
+        list.clear();
+
+
+        orderListAdapter = new OrderListAdapter(getActivity(), list);
+        binding.recycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+        binding.recycler.setAdapter(orderListAdapter);
+        binding.recycler.setHasFixedSize(false);
+        orderListAdapter.setOnClickItemListener((description, type, Inv_GUID) -> {
+
+            if (type == 1) {
+                Bundle bundleOrder = new Bundle();
+                bundleOrder.putString("Inv_GUID", Inv_GUID);
+                bundleOrder.putString("Tbl_GUID", "");
+                bundleOrder.putString("Tbl_NAME", "");
+                bundleOrder.putString("Ord_TYPE", "");
+                bundleOrder.putString("Acc_GUID", "");
+                bundleOrder.putString("Acc_NAME", "");
+                bundleOrder.putString("type", "1");
+
+                InVoiceDetailFragment inVoiceDetailFragment = new InVoiceDetailFragment();
+                inVoiceDetailFragment.setArguments(bundleOrder);
+                getActivity().getSupportFragmentManager().beginTransaction().add(R.id.frame_launcher, inVoiceDetailFragment, "InVoiceDetailFragment").addToBackStack("InVoiceDetailFX").commit();
+            } else {
+                Invoice invoice = new Invoice();
+                invoice.INV_UID = Inv_GUID;
+                invoice.INV_DESCRIBTION = description;
+                List<Invoice> listInvoice = new ArrayList<>();
+                listInvoice.add(invoice);
+
+
+                List<InvoiceDetail> invoiceDetailList = new ArrayList<>();
+                List<PaymentRecieptDetail> clsPaymentRecieptDetails = new ArrayList<>();
+                sendFeedBack(listInvoice, invoiceDetailList, clsPaymentRecieptDetails);
+            }
+
+
+        });
+
+
+        binding.ivBack.setOnClickListener(v -> {
+                    ((LauncherActivity) getActivity()).getVisibilityBottomBar(true);
+                    getActivity().getSupportFragmentManager().popBackStack();
+                }
+        );
+
+        getAllInvoice1(accGUID, datVip);
+
+
+    }
+
+
+    private void getAllInvoice1(String AccGuid, String date) {
+        if (!isNetworkAvailable(getActivity())) {
+            ShowErrorConnection();
+            return;
+        }
+
+        try {
+            compositeDisposable.add(
+                    api.getAllInvoice1(company.USER, company.PASS, AccGuid, date)
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .doOnSubscribe(disposable -> {
+                            })
+                            .subscribe(jsonElement -> {
+                                Gson gson = new Gson();
+                                Type typeIDs = new TypeToken<ModelInvoice>() {
+                                }.getType();
+                                ModelInvoice iDs = null;
+                                try {
+                                    iDs = gson.fromJson(jsonElement, typeIDs);
+                                } catch (Exception ignored) {
+                                    binding.txtError.setTextColor(getResources().getColor(R.color.medium_color));
+                                    binding.txtError.setVisibility(View.VISIBLE);
+                                    binding.txtError.setText("دریافت آخرین اطلاعات ناموفق");
+                                    binding.progressBar.setVisibility(View.GONE);
+                                }
+
+                                if (iDs != null) {
+
+                                    list.clear();
+
+                                    list.addAll(iDs.getInvoice());
+                                    if (list.size() == 0) {
+                                        binding.txtError.setTextColor(getResources().getColor(R.color.medium_color));
+                                        binding.txtError.setVisibility(View.VISIBLE);
+                                        binding.txtError.setText("هیچ سفارشی وجود ندارد");
+                                    }
+                                    orderListAdapter.notifyDataSetChanged();
+
+
+                                    // InvoiceDetail.saveInTx(iDs.getInvoiceDetail());
+
+                                } else {
+
+                                    binding.txtError.setTextColor(getResources().getColor(R.color.medium_color));
+                                    binding.txtError.setVisibility(View.VISIBLE);
+                                    binding.txtError.setText("دریافت آخرین اطلاعات ناموفق");
+                                    binding.progressBar.setVisibility(View.GONE);
+
+
+                                }
+                                binding.progressBar.setVisibility(View.GONE);
+
+
+                            }, throwable -> {
+
+                                binding.txtError.setTextColor(getResources().getColor(R.color.medium_color));
+                                binding.txtError.setVisibility(View.VISIBLE);
+                                binding.txtError.setText("خطا در دریافت اطلاعات فاکتور...");
+                                binding.progressBar.setVisibility(View.GONE);
+
+                            })
+            );
+        } catch (Exception e) {
+
+
+            binding.txtError.setTextColor(getResources().getColor(R.color.medium_color));
+            binding.txtError.setVisibility(View.VISIBLE);
+            binding.txtError.setText("خطا در دریافت اطلاعات فاکتور...");
+            binding.progressBar.setVisibility(View.GONE);
+        }
+
+    }
+
+    private boolean isNetworkAvailable(Activity activity) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        @SuppressLint("MissingPermission") NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    private void ShowErrorConnection() {
+        binding.progressBar.setVisibility(View.GONE);
+        textMessageDialog.setText("خطا در اتصال به اینترنت");
+        ivIconSync.setImageResource(R.drawable.ic_wifi);
+        btnNoDialog.setText("بستن");
+        btnOkDialog.setText("سینک مجدد");
+        dialogSync.dismiss();
+        dialogSync.show();
+
+
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        compositeDisposable.dispose();
+        binding = null;
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        compositeDisposable.clear();
+    }
+
+
+    private class JsonObject {
+        public List<Invoice> Invoice;
+        public List<ir.kitgroup.saleinOrder.DataBase.InvoiceDetail> InvoiceDetail;
+        public List<ir.kitgroup.saleinOrder.models.PaymentRecieptDetail> PaymentRecieptDetail;
+    }
+
+    private void sendFeedBack(List<Invoice> invoice, List<InvoiceDetail> invoiceDetail, List<PaymentRecieptDetail> clsPaymentRecieptDetail) {
+
+        if (!isNetworkAvailable(getActivity())) {
+            ShowErrorConnection();
+            return;
+        }
+        try {
+            customProgress.showProgress(getActivity(), "در حال ارسال سفارش", true);
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.Invoice = invoice;
+            jsonObject.InvoiceDetail = invoiceDetail;
+            jsonObject.PaymentRecieptDetail = clsPaymentRecieptDetail;
+
+
+            Gson gson = new Gson();
+            Type typeJsonObject = new TypeToken<JsonObject>() {
+            }.getType();
+            compositeDisposable.add(
+                    api.sendFeedBack(company.USER, company.PASS, gson.toJson(jsonObject, typeJsonObject))
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .doOnSubscribe(disposable -> {
+                            })
+                            .subscribe(jsonElement -> {
+
+                                customProgress.hideProgress();
+
+                                Gson gson1 = new Gson();
+                                Type typeIDs = new TypeToken<ModelLog>() {
+                                }.getType();
+                                ModelLog iDs = gson1.fromJson(jsonElement, typeIDs);
+                                int message = 0;
+
+                                if (iDs != null) {
+                                    message = iDs.getLogs().get(0).getMessage();
+
+                                }
+                                if (message == 1) {
+
+                                    AlertDialog alertDialog = new AlertDialog.Builder(getActivity())
+                                            .setMessage("نظر شما با موفقیت ارسال شد.")
+                                            .setPositiveButton("بستن", (dialog, which) -> {
+                                                dialog.dismiss();
+                                            })
+                                            .show();
+
+                                    TextView textView = (TextView) alertDialog.findViewById(android.R.id.message);
+                                    Typeface face = Typeface.createFromAsset(getActivity().getAssets(), "iransans.ttf");
+                                    textView.setTypeface(face);
+                                    textView.setTextColor(getResources().getColor(R.color.green_table));
+                                    textView.setTextSize(13);
+
+
+                                } else {
+                                    AlertDialog alertDialog = new AlertDialog.Builder(getActivity())
+                                            .setMessage("خطا در ارسال نظر")
+                                            .setPositiveButton("بستن", (dialog, which) -> {
+                                                dialog.dismiss();
+                                            })
+                                            .show();
+
+                                    TextView textView = (TextView) alertDialog.findViewById(android.R.id.message);
+                                    Typeface face = Typeface.createFromAsset(getActivity().getAssets(), "iransans.ttf");
+                                    textView.setTypeface(face);
+                                    textView.setTextColor(getResources().getColor(R.color.red_table));
+                                    textView.setTextSize(13);
+
+                                }
+
+
+                            }, throwable -> {
+
+                                customProgress.hideProgress();
+
+                                if (customProgress.isShow)
+                                    customProgress.hideProgress();
+                                Toast.makeText(getActivity(), "خطا در ارسال توضیحات", Toast.LENGTH_SHORT).show();
+
+
+                            })
+            );
+        } catch (Exception e) {
+            customProgress.hideProgress();
+
+            if (customProgress.isShow)
+                customProgress.hideProgress();
+            Toast.makeText(getActivity(), "خطا در ارسال توضیحات", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+}
