5994c1d75d1599f3398f09324b83d0bec5ce82ff
f.javadi <Farzane1!>
2021-12-27 17:14:31 +0330

    SaleIn

---
+++ app/src/main/java/ir/kitgroup/saleinmeat/models/Config.java
@@ -0,0 +1,11 @@
+package ir.kitgroup.saleinmeat.models;
+
+public class Config {
+    public  int imageLogo;
+    public  int mode;
+    public  String INSKU_ID;
+    public  int imageIcon;
+    public  String IP1;
+    public  String N;
+    public  String DESC;
+}
