e1f2341b08784b9381de776982c16777ed0af237
f.javadi <Farzane1!>
2021-12-28 13:59:04 +0330

    SaleIn

---
+++ app/src/main/java/ir/kitgroup/saleinjam/models/ModelLog.java
@@ -0,0 +1,16 @@
+package ir.kitgroup.saleinjam.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ModelLog {
+    @SerializedName("Log")
+    @Expose
+    private List<Log> logs = null;
+    public List<Log> getLogs() {
+        return logs;
+    }
+
+}
