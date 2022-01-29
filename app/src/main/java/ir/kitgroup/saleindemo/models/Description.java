21c3c00399859e58956a213f1623fd1423b09de8
f.javadi <Farzane1!>
2022-01-29 11:59:21 +0330

    SaleIn

---
+++ app/src/main/java/ir/kitgroup/saleindemo/models/Description.java
@@ -0,0 +1,6 @@
+package ir.kitgroup.saleindemo.models;
+
+public class Description {
+    public String DSC;
+    public boolean Click;
+}
--- app/src/main/java/ir/kitgroup/saleinmeat/models/ModelDate.java
+++
@@ -1,8 +0,0 @@
-package ir.kitgroup.saleinmeat.models;
-
-import java.util.Date;
-
-public class ModelDate {
-    public  Boolean Click=false;
-    public Date date;
-}
