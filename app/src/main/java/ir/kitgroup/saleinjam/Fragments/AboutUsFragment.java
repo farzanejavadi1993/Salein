de6eb17c9e7cb9bc780bf3418832ad649f06d7ae
f.javadi <Farzane1!>
2022-02-01 15:26:13 +0330

    SaleIn

---
+++ app/src/main/java/ir/kitgroup/saleinjam/Fragments/AboutUsFragment.java
@@ -0,0 +1,46 @@
+package ir.kitgroup.saleinjam.Fragments;
+
+import android.graphics.text.LineBreaker;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.orm.query.Select;
+
+import ir.kitgroup.saleinjam.Activities.LauncherActivity;
+import ir.kitgroup.saleinjam.DataBase.Company;
+import ir.kitgroup.saleinjam.databinding.AboutUsFragmentBinding;
+
+
+public class AboutUsFragment extends Fragment {
+
+
+    private AboutUsFragmentBinding binding;
+
+    private Company company;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = AboutUsFragmentBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ((LauncherActivity) getActivity()).getVisibilityBottomBar(false);
+
+        company= Select.from(Company.class).first();
+        binding.txtDescription.setText(company!=null && company.ABUS !=null?company.ABUS :"");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            binding.txtDescription.setJustificationMode(LineBreaker.JUSTIFICATION_MODE_INTER_WORD);
+        }
+
+    }
+}
