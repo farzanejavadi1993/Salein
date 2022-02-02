64c9a45a2ac9a309198f1c3a3eea1d0e6b01b3a0
f.javadi <Farzane1!>
2022-02-02 09:52:40 +0330

    SaleIn

--- app/src/main/java/ir/kitgroup/saleinkonj/models/Description.java
+++
@@ -1,6 +0,0 @@
-package ir.kitgroup.saleinkonj.models;
-
-public class Description {
-    public String DSC;
-    public boolean Click;
-}
--- app/src/main/java/ir/kitgroup/saleinkonj/models/Image.java
+++
@@ -1,6 +0,0 @@
-package ir.kitgroup.saleinkonj.models;
-
-public class Image {
-    public String IMG;
-    public String I;
-}
