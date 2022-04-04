4925df2e9caec9e752233f3b48af8bfca72e3359
f.javadi <Farzane1!>
2022-04-04 15:30:10 +0430

    SaleIn

---
+++ app/src/main/java/ir/kitgroup/saleinOrder/Fragments/AboutUsFragment.java
@@ -0,0 +1,63 @@
+package ir.kitgroup.saleinOrder.Fragments;
+
+import android.graphics.text.LineBreaker;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.orm.query.Select;
+import com.squareup.picasso.Picasso;
+
+import ir.kitgroup.saleinOrder.Activities.LauncherActivity;
+import ir.kitgroup.saleinOrder.DataBase.Company;
+import ir.kitgroup.saleinOrder.R;
+import ir.kitgroup.saleinOrder.databinding.AboutUsFragmentBinding;
+
+
+public class AboutUsFragment extends Fragment {
+
+
+    private AboutUsFragmentBinding binding;
+
+    private Company company;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = AboutUsFragmentBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ((LauncherActivity) getActivity()).getVisibilityBottomBar(false);
+
+        company= Select.from(Company.class).first();
+        binding.txtDescription.setText(company!=null && company.ABUS !=null?company.ABUS :"");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            binding.txtDescription.setJustificationMode(LineBreaker.JUSTIFICATION_MODE_INTER_WORD);
+        }
+
+        binding.ivBackFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+        Picasso.get()
+                .load("http://api.kitgroup.ir/GetCompanyImage?id=" +
+                        company.I+"&width=300&height=300")
+                .error(R.drawable.loading)
+                .placeholder(R.drawable.loading)
+                .into(binding.ivLogo);
+
+    }
+}
---
+++ app/src/main/java/ir/kitgroup/saleinOrder/models/ModelDate.java
@@ -0,0 +1,8 @@
+package ir.kitgroup.saleinOrder.models;
+
+import java.util.Date;
+
+public class ModelDate {
+    public  Boolean Click=false;
+    public Date date;
+}
